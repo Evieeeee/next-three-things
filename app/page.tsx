b9022200c0bfc3c8c87b855c1bbf5bc0527a7f26
Evieeeee <100174760+Evieeeee@users.noreply.github.com>
2025-09-09 16:11:44 +1000

    Update page.tsx
    Include chat bot

--- app/page.tsx
+++ app/page.tsx
@@ -47,6 +47,31 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
+<script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+<df-messenger
+  project-id="contentclicks"
+  agent-id="15cc4939-9e8d-48d4-a321-3486e28d1b5d"
+  language-code="en"
+  max-query-length="-1"
+  allow-feedback="all">
+  <df-messenger-chat-bubble
+    chat-title="ContentClicks Chat">
+  </df-messenger-chat-bubble>
+</df-messenger>
+<style>
+  df-messenger {
+    z-index: 999;
+    position: fixed;
+    --df-messenger-font-color: #000;
+    --df-messenger-font-family: Google Sans;
+    --df-messenger-chat-background: #FFFFFF;
+    --df-messenger-message-user-background: #e9e0c9;
+    --df-messenger-message-bot-background: #a37e2c;
+    bottom: 16px;
+    right: 16px;
+  }
+</style>
     </main>
   );
 }
